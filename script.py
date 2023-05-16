c19d04657e86a70fac7ed96b5b2fec6134d3a34a
Nirav Patel <64038962+Niravanaa@users.noreply.github.com>
2023-05-16 15:59:46 -0400

    Added the Python script

---
+++ script.py
@@ -0,0 +1,46 @@
+import docx
+from docx2pdf import convert
+import os
+
+
+def replace_placeholder(doc, placeholder, replacement):
+    for p in doc.paragraphs:
+        if placeholder in p.text:
+            inline = p.runs
+            for item in range(len(inline)):
+                if placeholder in inline[item].text:
+                    inline[item].text = inline[item].text.replace(placeholder,
+                                                                  replacement)
+
+
+def main():
+    while True:
+        filename = "coverletter.docx"
+        doc = docx.Document(filename)
+        placeholders = {
+            "[RecipientName]": "",
+            "[RecipientTitle]": "",
+            "[Position]": "",
+            "[CompanyName]": "",
+            "[CompanyAddress]": "",
+            "[Address2]": ""
+        }
+        for placeholder in placeholders:
+            replacement = input(f"Enter {placeholder}: ")
+            placeholders[placeholder] = replacement
+            replace_placeholder(doc, placeholder, replacement)
+        doc.save(
+            placeholders["[CompanyName]"] + "_" +
+            placeholders["[Position]"] + ".docx"
+        )
+        convert(placeholders["[CompanyName]"] + "_" +
+                placeholders["[Position]"] + ".docx")
+        os.remove(placeholders["[CompanyName]"] + "_" +
+                  placeholders["[Position]"] + ".docx")
+        choice = input("Press q to quit or any other key to continue: ")
+        if choice.lower() == "q":
+            break
+
+
+if __name__ == "__main__":
+    main()
